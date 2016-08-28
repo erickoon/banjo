00a802b85ad2a73a00d0566421389b91420d675f
Eric Koon <erickoon@gmail.com>
2016-08-28 10:56:21 -0700

    webapp init

---
+++ tweet-webapp/src/main/java/com/banjo/tweetwebapp/TweetWebappApplication.java
@@ -0,0 +1,12 @@
+package com.banjo.tweetwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TweetWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TweetWebappApplication.class, args);
+	}
+}
